54f59b59924f865e08ea63627acced77a5d99c62
Bin Song <Bin.Song@mpip-mainz.mpg.de>
2018-08-17 15:16:24 +0200

    Work on ExtPlumed documentation

--- src/integrator/ExtPlumed.py
+++ src/integrator/ExtPlumed.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2017,2018
+#  Copyright (C) 2018
 #      Max Planck Institute for Polymer Research
 #
 #  This file is part of ESPResSo++.
@@ -18,31 +18,43 @@
 
 
 r"""
-******************************
+*************************************************
 espressopp.integrator.ExtPlumed
-******************************
+*************************************************
 
-To call PLUMED (PLUgin for MolEcular Dynamics), set environment variable PLUMED_KERNEL to the full path of
-libplumedKernel.so, and add the path of the directory of libplumed.so to environment variable LD_LIBRARY_PATH, when
-PLUMED is linked through runtime linking. If PLUMED is linked by dynamic linking, i.e., it is linked as a shared
-library, PLUMED_KERNEL needs not to be set. If PLUMED is linked statically, neither environment variable needs to be
-changed. By default, PLUMED is linked by runtime linking. Error messages related to "undefined symbol" can be ignored,
-as long as the simulation runs.
+This extension serves as interface between ESPResSo++ and `PLUMED <https://plumed.github.io/>`_. PLUMED can
+be used to run realtime analysis and bias the simulation along chosen collective variables. Details of using
+PLUMED and its input file can be found on its website.
+
+To call PLUMED, set environment variable :envvar:`PLUMED_KERNEL` to the full path of libplumedKernel.so, and add
+the path of the directory of libplumed.so to environment variable :envvar:`LD_LIBRARY_PATH`, when PLUMED is linked through runtime linking.
+If PLUMED is linked by dynamic linking, i.e., it is linked as a shared library to ESPResSo++, :envvar:`PLUMED_KERNEL` needs not to be set.
+If PLUMED is linked statically, neither environment variable needs to be provided.
+Error messages regarding to "undefined symbol" can be ignored, as long as the simulation runs.
+
+By default, PLUMED is linked by runtime linking. The user can change the way of PLUMED linking to ESPResSo++ by changing CMake variable :makevar:`PLUMED_LINK_TYPE`
+to either "shared" or "static" from the default "runtime". If your distribution of PLUMED is not installed in a default system directory, you can define the root
+path of PLUMED installation as CMake variable :makevar:`PLUMED_HOME`. If you keep the default "runtime" linking, your installation of PLUMED is not searched. Hence, you
+do not need to worry about whether your PLUMED is not installed in a default system directory or setting :makevar:`PLUMED_HOME`.
 
 usage:
 
-plumed = espressopp.integrator.ExtPlumed(system, "plumed.dat", "log.plumed", 0.005)
-plumed.setNaturalUnits()
-plumed.Init()
-integrator.addExtension(plumed)
+.. code:: python
+
+    plumed = espressopp.integrator.ExtPlumed(system, "plumed.dat", "log.plumed", 0.005)
+    plumed.setNaturalUnits()
+    plumed.Init()
+    integrator.addExtension(plumed)
 
 or:
 
-plumed = espressopp.integrator.ExtPlumed(system, "plumed.dat", "log.plumed", 0.005)
-plumed.setNaturalUnits()
-plumed.setRestart(1)
-plumed.Init()
-integrator.addExtension(plumed)
+.. code:: python
+
+    plumed = espressopp.integrator.ExtPlumed(system, "plumed.dat", "log.plumed", 0.005)
+    plumed.setNaturalUnits()
+    plumed.setRestart(1)
+    plumed.Init()
+    integrator.addExtension(plumed)
 
 .. function:: espressopp.integrator.ExtPlumed(system, cmd, log, dt)
 
